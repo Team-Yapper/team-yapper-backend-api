d58bc331be9b43171e1aa3fdefc335360b0a8a37
joshuabenavidesV <josbenavides358@gmail.com>
2025-12-05 15:48:03 -0600

    reverting back test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -22,7 +22,7 @@ def test_read_post_success(client, session):
     assert response.status_code == 200
     assert response.json() == {
         "id": post.id,
-        "content": "",
+        "content": "Hello this is my yapper post!",
         "user": {
             "id": user.id,
             "email": "test@example.com"
